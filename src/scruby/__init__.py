be5e8e9a5631f151c400d68e2a8a3b3c5321f129
kebasyaty <kebasyaty@gmail.com>
2025-09-26 18:35:57 +0300

    update docstring

--- src/scruby/__init__.py
+++ src/scruby/__init__.py
@@ -1,8 +1,7 @@
-"""A fast key-value storage library.
+"""Scruby is an asynchronous library for building and managing the database, by scheme of key-value.
 
-Scruby is a fast key-value storage asynchronous library that provides an
-ordered mapping from string keys to string values.
-The library uses fractal-tree addressing.
+The library uses fractal-tree addressing and
+the search for documents based on the effect of a quantum loop.
 
 The database consists of collections.
 The maximum size of the one collection is 16**8=4294967296 branches,
@@ -11,7 +10,8 @@ each branch can store one or more keys.
 The value of any key in collection can be obtained in 8 steps,
 thereby achieving high performance.
 
-In the future, to search by value of key, the use of a quantum loop is supposed.
+The effectiveness of the search for documents based on a quantum loop,
+requires a large number of processor threads.
 """
 
 from __future__ import annotations
