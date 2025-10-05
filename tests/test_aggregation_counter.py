4b0f99b87a5174121c1f056f74d45604f8fc5ae5
kebasyaty <kebasyaty@gmail.com>
2025-10-05 12:50:03 +0300

    update test_aggregation_counter

--- tests/test_aggregation_counter.py
+++ tests/test_aggregation_counter.py
@@ -54,6 +54,7 @@ def task_counter(
             docs = future.result(timeout)
             for doc in docs:
                 if counter.check():
+                    # [:limit_docs] - Control overflow in a multithreaded environment.
                     return users[:limit_docs]
                 users.append(doc)
                 counter.next()
