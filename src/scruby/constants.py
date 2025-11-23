5ce85dc21c22ae31ccd8214424dccab8f47a8de0
kebasyaty <kebasyaty@gmail.com>
2025-11-23 14:58:00 +0200

    fix docstring to a `constants` module

--- src/scruby/constants.py
+++ src/scruby/constants.py
@@ -2,7 +2,7 @@
 
 The module contains the following variables:
 
-- `DB_ROOT` - Path to root directory of database. `By default = "ScrubyDB"` (*in root of project*).
+- `DB_ROOT` - Path to root directory of database. `By default = "ScrubyDB" (in root of project)`.
 - `HASH_REDUCE_LEFT` - The length of the hash reduction on the left side.
     - `0` - 4294967296 branches in collection.
     - `2` - 16777216 branches in collection.
