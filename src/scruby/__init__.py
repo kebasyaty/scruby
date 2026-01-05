623a1d133a5c19573156657a95ed51ddf0cc0390
kebasyaty <kebasyaty@gmail.com>
2026-01-05 13:29:46 +0200

    update comment

--- src/scruby/__init__.py
+++ src/scruby/__init__.py
@@ -1,11 +1,8 @@
 #
-# .|'''|                        '||
-# ||                             ||
-# `|'''|, .|'', '||''| '||  ||`  ||''|, '||  ||`
-#  .   || ||     ||     ||  ||   ||  ||  `|..||
-#  |...|' `|..' .||.    `|..'|. .||..|'      ||
-#                                         ,  |'
-#                                          ''
+# .dP"Y8  dP""b8 88""Yb 88   88 88""Yb Yb  dP
+# `Ybo." dP   `" 88__dP 88   88 88__dP  YbdP
+# o.`Y8b Yb      88"Yb  Y8   8P 88""Yb   8P
+# 8bodP'  YboodP 88  Yb `YbodP' 88oodP  dP
 #
 # Copyright (c) 2025 Gennady Kostyunin
 # SPDX-License-Identifier: MIT
