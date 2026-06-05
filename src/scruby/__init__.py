9f1ae2dd371e956ce3c616fcf02b7e791e477457
develgen <develgen.dev@gmail.com>
2026-06-05 12:38:28 +0300

    update comment

--- src/scruby/__init__.py
+++ src/scruby/__init__.py
@@ -1,7 +1,8 @@
-# .dP"Y8  dP""b8 88""Yb 88   88 88""Yb Yb  dP
-# `Ybo." dP   `" 88__dP 88   88 88__dP  YbdP
-# o.`Y8b Yb      88"Yb  Y8   8P 88""Yb   8P
-# 8bodP'  YboodP 88  Yb `YbodP' 88oodP  dP
+#    .dMMMb  .aMMMb  dMMMMb  dMP dMP dMMMMb  dMP dMP
+#   dMP" VP dMP"VMP dMP.dMP dMP dMP dMP"dMP dMP.dMP
+#   VMMMb  dMP     dMMMMK" dMP dMP dMMMMK"  VMMMMP
+# dP .dMP dMP.aMP dMP"AMF dMP.aMP dMP.aMF dA .dMP
+# VMMMP"  VMMMP" dMP dMP  VMMMP" dMMMMP"  VMMMP"
 #
 # Copyright (c) 2025 Gennady Kostyunin
 # SPDX-License-Identifier: MIT
