e0bd8a05f73ec9abfcb6c2b8602d8cf5a4b8b96b
develgen <develgen.dev@gmail.com>
2026-06-10 18:00:08 +0300

    update load_cache

--- src/scruby/cache.py
+++ src/scruby/cache.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 __all__ = ("DocCache",)
 
-from typing import Any, ClassVar, Literal, final
+from typing import Any, ClassVar, Literal, Never, assert_never, final
 
 from scruby.config import ScrubyConfig
 
@@ -22,4 +22,15 @@ class DocCache:
         """Load all documents from the database into the cache."""
         db_root: str = ScrubyConfig.db_root  # noqa: F841
         db_id: str | None = ScrubyConfig.db_id  # noqa: F841
-        HASH_REDUCE_LEFT: Literal[4, 6] = ScrubyConfig.HASH_REDUCE_LEFT  # noqa: F841
+        HASH_REDUCE_LEFT: Literal[4, 6] = ScrubyConfig.HASH_REDUCE_LEFT
+        max_number_branch: int = 0
+        # Get maximum number of branches.
+        match HASH_REDUCE_LEFT:
+            case 4:
+                max_number_branch = 65536
+            case 6:
+                max_number_branch = 256
+            case _ as unreachable:
+                assert_never(Never(unreachable))  # pyrefly: ignore[not-callable]
+        #
+        branch_numbers: range = range(max_number_branch)  # noqa: F841
