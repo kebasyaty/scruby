e96db69c4156c377f52606ee7dcc4ef0aea283f2
develgen <develgen.dev@gmail.com>
2026-06-10 15:14:21 +0300

    update comment for doc_cache

--- src/scruby/cache.py
+++ src/scruby/cache.py
@@ -7,5 +7,6 @@ __all__ = ("doc_cache",)
 from typing import Any
 
 # Cache documents to optimize work with the database.
-# Cache structure: {"CollectionName": {"hash_symbol": {"hash_symbol": }}}
+# Cache structure:
+# {"CollectionName": {"hash_symbol": {"hash_symbol": ...{"hash_symbol": leaf.json}}}
 doc_cache: dict[str, Any] = {}
