91638caac6fdef35d80e41ac380b0c3672c9d841
develgen <develgen.dev@gmail.com>
2026-06-10 15:24:06 +0300

    add DocCache

--- src/scruby/cache.py
+++ src/scruby/cache.py
@@ -2,11 +2,15 @@
 
 from __future__ import annotations
 
-__all__ = ("doc_cache",)
+__all__ = ("DocCache",)
 
-from typing import Any
+from typing import Any, ClassVar, final
 
-# Cache documents to optimize work with the database.
-# Cache structure:
-# {"CollectionName": {"hash_symbol": {"hash_symbol": ...{"hash_symbol": leaf.json}}}
-doc_cache: dict[str, Any] = {}
+
+@final
+class DocCache:
+    """Cache documents to optimize work with the database."""
+
+    # Cache structure:
+    # {"CollectionName": {"hash_symbol": {"hash_symbol": ...{"hash_symbol": leaf.json}}}
+    cache: ClassVar[dict[str, Any]] = {}
