9bb5e04bb39c89b1741717c15ea302725ea951b7
develgen <develgen.dev@gmail.com>
2026-06-17 17:21:32 +0300

    update test_hash_reduce_left_0

--- tests/test_hash_reduce_left_0.py
+++ tests/test_hash_reduce_left_0.py
@@ -2,173 +2,174 @@
 
 from __future__ import annotations
 
-from scruby import Scruby, ScrubyConfig
-from scruby.utils import get_from_env
+from datetime import datetime
+from typing import Annotated
+from zoneinfo import ZoneInfo
+
+import pytest
+from pydantic import EmailStr, Field
+from pydantic_extra_types.phone_numbers import PhoneNumber, PhoneNumberValidator
+
+from scruby import Scruby, ScrubyModel
+
+pytestmark = pytest.mark.asyncio(loop_scope="module")
 
 # Delete DB.
 # Hint: If the previous test failed and the database remains.
 Scruby.napalm()
 
 
-class TestConfigParams:
-    """Testing configuration parameters."""
+class User(ScrubyModel):
+    """User model."""
 
-    def test_db_id(self) -> None:
-        """ScrubyConfig.db_id."""
-        assert ScrubyConfig.db_id is None
-
-    def test_db_root(self) -> None:
-        """Test a DB_ROOT parameter."""
-        assert ScrubyConfig.db_root == "ScrubyDB"
-
-    def test_hash_reduce_left(self) -> None:
-        """Test a HASH_REDUCE_LEFT parameter."""
-        assert ScrubyConfig.HASH_REDUCE_LEFT == 7
-
-    def test_max_number_branch(self) -> None:
-        """Test a MAX_NUMBER_BRANCH parameter."""
-        assert ScrubyConfig.MAX_NUMBER_BRANCH == 16
-
-    def test_max_workers(self) -> None:
-        """Test a MAX_WORKERS parameter."""
-        assert ScrubyConfig.max_workers is None
-
-    def test_plugins(self) -> None:
-        """Test a PLUGINS parameter."""
-        assert ScrubyConfig.plugins is None
+    first_name: str = Field(strict=True)
+    last_name: str = Field(strict=True)
+    birthday: datetime = Field(strict=True)
+    email: EmailStr = Field(strict=True)
+    phone: Annotated[PhoneNumber, PhoneNumberValidator(number_format="E164")] = Field(frozen=True)
+    # key is always at bottom
+    key: str = Field(
+        strict=True,
+        frozen=True,
+        default_factory=lambda data: data["phone"],
+    )
 
 
-class TestConfigMethods:
-    """Testing configuration methods."""
+class Phone(ScrubyModel):
+    """Phone model."""
 
-    def test_init_db_id(self) -> None:
-        """Test a init_db_id method."""
-        ScrubyConfig.init_db_id()
-        assert ScrubyConfig.db_id is not None
-        assert len(ScrubyConfig.db_id) == 8
-        delimiter: str = "/" if ScrubyConfig.sys_platform != "win32" else ""
-        db_id = get_from_env(key="db_id", dotenv_path=f"{ScrubyConfig.db_root}{delimiter}.env.meta")
-        assert db_id == ScrubyConfig.db_id
-        #
-        # Delete DB.
-        Scruby.napalm()
+    brand: str = Field(strict=True, frozen=True)
+    model: str = Field(strict=True, frozen=True)
+    screen_diagonal: float = Field(strict=True)
+    matrix_type: str = Field(strict=True)
+    # key is always at bottom
+    key: str = Field(
+        strict=True,
+        frozen=True,
+        default_factory=lambda data: f"{data['brand']}:{data['model']}",
+    )
 
-    def test_init_max_number_branch(self) -> None:
-        """Test a init_max_number_branch method."""
-        # 7
-        ScrubyConfig.HASH_REDUCE_LEFT = 7
-        ScrubyConfig.init_max_number_branch()
-        assert ScrubyConfig.MAX_NUMBER_BRANCH == 16
-        # 6
-        ScrubyConfig.HASH_REDUCE_LEFT = 6
-        ScrubyConfig.init_max_number_branch()
-        assert ScrubyConfig.MAX_NUMBER_BRANCH == 256
-        # 5
-        ScrubyConfig.HASH_REDUCE_LEFT = 5
-        ScrubyConfig.init_max_number_branch()
-        assert ScrubyConfig.MAX_NUMBER_BRANCH == 4096
-        # 0
-        ScrubyConfig.HASH_REDUCE_LEFT = 0
-        ScrubyConfig.init_max_number_branch()
-        assert ScrubyConfig.MAX_NUMBER_BRANCH == 4294967296
-        #
-        # Delete DB.
-        Scruby.napalm()
 
-    def test_check_hash_reduce_left(self) -> None:
-        """Test a check_hash_reduce_left method."""
-        # 7
-        test_num = 7
-        ScrubyConfig.HASH_REDUCE_LEFT = test_num
-        ScrubyConfig.check_hash_reduce_left()
-        delimiter: str = "/" if ScrubyConfig.sys_platform != "win32" else ""
-        hash_reduce_left = get_from_env(
-            key="hash_reduce_left",
-            dotenv_path=f"{ScrubyConfig.db_root}{delimiter}.env.meta",
+class Car(ScrubyModel):
+    """Car model."""
+
+    brand: str = Field(strict=True, frozen=True)
+    model: str = Field(strict=True, frozen=True)
+    year: int = Field(strict=True)
+    power_reserve: int = Field(strict=True)
+    # key is always at bottom
+    key: str = Field(
+        strict=True,
+        frozen=True,
+        default_factory=lambda data: f"{data['brand']}:{data['model']}",
+    )
+
+
+# Activate database.
+Scruby.run()
+
+
+async def test_create_db() -> None:
+    """Create a test database."""
+    # Create users
+    user_coll = await Scruby.collection(User)
+    for num in range(9):
+        user = User(
+            first_name="John",
+            last_name="Smith",
+            birthday=datetime(1970, 1, 1, tzinfo=ZoneInfo("UTC")),
+            email="John_Smith@gmail.com",
+            phone=f"+44798612345{num}",
         )
-        assert hash_reduce_left is not None
-        assert int(hash_reduce_left) == test_num
-        # repeat
-        ScrubyConfig.check_hash_reduce_left()
-        delimiter: str = "/" if ScrubyConfig.sys_platform != "win32" else ""
-        hash_reduce_left = get_from_env(
-            key="hash_reduce_left",
-            dotenv_path=f"{ScrubyConfig.db_root}{delimiter}.env.meta",
+        # Add user to collection.
+        await user_coll.add_doc(user)
+
+    # Create phones
+    phone_coll = await Scruby.collection(Phone)
+    for num in range(9):
+        phone = Phone(
+            brand="Samsung",
+            model=f"Galaxy A26 {num}",
+            screen_diagonal=6.7,
+            matrix_type="Super AMOLED",
         )
-        assert hash_reduce_left is not None
-        assert int(hash_reduce_left) == test_num
-        #
-        # Delete DB.
-        Scruby.napalm()
-        #
-        # 6
-        test_num = 6
-        ScrubyConfig.HASH_REDUCE_LEFT = test_num
-        ScrubyConfig.check_hash_reduce_left()
-        delimiter: str = "/" if ScrubyConfig.sys_platform != "win32" else ""
-        hash_reduce_left = get_from_env(
-            key="hash_reduce_left",
-            dotenv_path=f"{ScrubyConfig.db_root}{delimiter}.env.meta",
+        # Add phone to collection.
+        await phone_coll.add_doc(phone)
+
+    # Create cars
+    car_coll = await Scruby.collection(Car)
+    for num in range(9):
+        car = Car(
+            brand="Mazda",
+            model=f"EZ-6 {num}",
+            year=2025,
+            power_reserve=600,
         )
-        assert hash_reduce_left is not None
-        assert int(hash_reduce_left) == test_num
-        # repeat
-        ScrubyConfig.check_hash_reduce_left()
-        delimiter: str = "/" if ScrubyConfig.sys_platform != "win32" else ""
-        hash_reduce_left = get_from_env(
-            key="hash_reduce_left",
-            dotenv_path=f"{ScrubyConfig.db_root}{delimiter}.env.meta",
-        )
-        assert hash_reduce_left is not None
-        assert int(hash_reduce_left) == test_num
-        #
-        # Delete DB.
-        Scruby.napalm()
-        #
-        # 5
-        test_num = 5
-        ScrubyConfig.HASH_REDUCE_LEFT = test_num
-        ScrubyConfig.check_hash_reduce_left()
-        delimiter: str = "/" if ScrubyConfig.sys_platform != "win32" else ""
-        hash_reduce_left = get_from_env(
-            key="hash_reduce_left",
-            dotenv_path=f"{ScrubyConfig.db_root}{delimiter}.env.meta",
-        )
-        assert hash_reduce_left is not None
-        assert int(hash_reduce_left) == test_num
-        # repeat
-        ScrubyConfig.check_hash_reduce_left()
-        delimiter: str = "/" if ScrubyConfig.sys_platform != "win32" else ""
-        hash_reduce_left = get_from_env(
-            key="hash_reduce_left",
-            dotenv_path=f"{ScrubyConfig.db_root}{delimiter}.env.meta",
-        )
-        assert hash_reduce_left is not None
-        assert int(hash_reduce_left) == test_num
-        #
-        # Delete DB.
-        Scruby.napalm()
-        #
-        # 0
-        test_num = 0
-        ScrubyConfig.HASH_REDUCE_LEFT = test_num
-        ScrubyConfig.check_hash_reduce_left()
-        delimiter: str = "/" if ScrubyConfig.sys_platform != "win32" else ""
-        hash_reduce_left = get_from_env(
-            key="hash_reduce_left",
-            dotenv_path=f"{ScrubyConfig.db_root}{delimiter}.env.meta",
-        )
-        assert hash_reduce_left is not None
-        assert int(hash_reduce_left) == test_num
-        # repeat
-        ScrubyConfig.check_hash_reduce_left()
-        delimiter: str = "/" if ScrubyConfig.sys_platform != "win32" else ""
-        hash_reduce_left = get_from_env(
-            key="hash_reduce_left",
-            dotenv_path=f"{ScrubyConfig.db_root}{delimiter}.env.meta",
-        )
-        assert hash_reduce_left is not None
-        assert int(hash_reduce_left) == test_num
-        #
-        # Delete DB.
-        Scruby.napalm()
+        # Add car to collection
+        await car_coll.add_doc(car)
+
+
+async def test_hash_reduce_left_0() -> None:
+    """Testing the parameter hash_reduce_left = 0."""
+    # Get collection `User`.
+    user_coll = await Scruby.collection(User)
+
+    # collection_name
+    assert user_coll.collection_name() == "User"
+
+    # collection_list
+    coll_list = await Scruby.collection_list()
+    assert coll_list is not None
+    for coll_name in coll_list:
+        assert coll_name in ["User", "Phone", "Car"]
+
+    # estimated_document_count
+    assert await user_coll.estimated_document_count() == 9
+
+    # add_doc
+    user = User(
+        first_name="John",
+        last_name="Smith",
+        birthday=datetime(1970, 1, 1, tzinfo=ZoneInfo("UTC")),
+        email="John_Smith@gmail.com",
+        phone="+447986123459",
+    )
+    await user_coll.add_doc(user)
+    assert await user_coll.estimated_document_count() == 10
+
+    # update_doc and get_doc
+    user = User(
+        first_name="John",
+        last_name="Smith",
+        birthday=datetime(1972, 11, 7, tzinfo=ZoneInfo("UTC")),
+        email="John_Smith@gmail.com",
+        phone="+447986123459",
+    )
+    await user_coll.update_doc(user)
+    user: User | None = await user_coll.get_doc("+447986123459")
+    assert user is not None
+    assert user.birthday == datetime(1972, 11, 7, tzinfo=ZoneInfo("UTC"))
+
+    # has_key
+    assert await user_coll.has_key("+447986123459")
+
+    # delete_doc
+    await user_coll.delete_doc("+447986123459")
+    assert not await user_coll.has_key("+447986123459")
+    assert await user_coll.estimated_document_count() == 9
+    #
+    # delete_collection
+    await Scruby.delete_collection("User")
+    coll_list = await Scruby.collection_list()
+    assert coll_list is not None
+    for coll_name in coll_list:
+        assert coll_name in ["Phone", "Car"]
+    user_coll = await Scruby.collection(User)
+    assert await user_coll.estimated_document_count() == 0
+    coll_list = await Scruby.collection_list()
+    assert coll_list is not None
+    for coll_name in coll_list:
+        assert coll_name in ["User", "Phone", "Car"]
+    #
+    # Delete DB.
+    Scruby.napalm()
