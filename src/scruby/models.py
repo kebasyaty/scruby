7b73d1518ba6a841a6d4a92a8d6045ac1f10e311
develgen <develgen.dev@gmail.com>
2026-07-06 09:37:12 +0300

    update a password_is_valid method

--- src/scruby/models.py
+++ src/scruby/models.py
@@ -109,11 +109,11 @@ class CryptModel(BaseModel):
         """
         assert isinstance(value, (str, SecretStr)), "Valid type: str | SecretStr"
         #
-        try:
-            value_hashed = self.hash_raw_password(value)
-            return bcrypt.checkpw(value_hashed.encode("utf-8"), self.password.encode("utf-8"))
-        except AttributeError:
+        if not bool(self.password):
             return False
+        #
+        value_hashed = self.hash_raw_password(value)
+        return bcrypt.checkpw(value_hashed.encode("utf-8"), self.password.encode("utf-8"))
 
     def update_password(
         self,
